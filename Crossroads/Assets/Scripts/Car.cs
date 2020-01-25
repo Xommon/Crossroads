ea559def440fa82b1330be88e2ac20949298eda4
Micheal Quentin <mquenti3@gmail.com>
2020-01-24 22:36:41 -0600

    Car Accelerates and Drives Off

---
+++ Crossroads/Assets/Scripts/Car.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public float speed;
+    public float maxSpeed;
+    public bool go;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 0;
+        maxSpeed = 7f;
+        go = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (go == true)
+        {
+            if (speed < maxSpeed)
+            {
+                // Accelerate
+                speed += 0.1f;
+            }
+            else
+            {
+                speed = maxSpeed;
+            }
+
+            transform.position += new Vector3((speed / 100), -(speed / 200), 0);
+        }
+    }
+}
