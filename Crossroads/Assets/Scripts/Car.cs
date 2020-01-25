77629e6b2d504e57f7e9a7e1b4becd63a36b0a50
Micheal Quentin <mquenti3@gmail.com>
2020-01-24 22:57:54 -0600

    Car Changes Direction/Movement/Sprite

--- Crossroads/Assets/Scripts/Car.cs
+++ Crossroads/Assets/Scripts/Car.cs
@@ -6,14 +6,39 @@ public class Car : MonoBehaviour
 {
     public float speed;
     public float maxSpeed;
+    public string direction;
     public bool canGo;
+
+    // Sprites
+    public SpriteRenderer sr;
+    public Sprite downRight;
+    public Sprite upRight;
+    public Sprite downLeft;
+    public Sprite upLeft;
 
     // Start is called before the first frame update
     void Start()
     {
         speed = 0;
-        maxSpeed = 7f;
+        maxSpeed = 9f;
         canGo = true;
+
+        if (transform.position.x < 0 && transform.position.y >= 0)
+        {
+            direction = "DR";
+        }
+        else if (transform.position.x < 0 && transform.position.y < 0)
+        {
+            direction = "UR";
+        }
+        else if (transform.position.x >= 0 && transform.position.y < 0)
+        {
+            direction = "UL";
+        }
+        else if (transform.position.x >= 0 && transform.position.y >= 0)
+        {
+            direction = "DL";
+        }
     }
 
     // Update is called once per frame
@@ -24,7 +49,7 @@ public class Car : MonoBehaviour
             if (speed < maxSpeed)
             {
                 // Accelerate
-                speed += 0.1f;
+                speed += 0.2f;
             }
             else
             {
@@ -35,8 +60,8 @@ public class Car : MonoBehaviour
         {
             if (speed > 0)
             {
-                // Deccelerate
-                speed -= 0.1f;
+                // Decelerate
+                speed -= 0.4f;
             }
             else
             {
@@ -44,6 +69,25 @@ public class Car : MonoBehaviour
             }
         }
 
-        transform.position += new Vector3((speed / 100), -(speed / 200), 0);
+        if (direction == "DR")
+        {
+            transform.position += new Vector3((speed / 100), -(speed / 200), 0);
+            sr.sprite = downRight;
+        }
+        else if (direction == "UR")
+        {
+            transform.position += new Vector3((speed / 100), (speed / 200), 0);
+            sr.sprite = upRight;
+        }
+        else if (direction == "UL")
+        {
+            transform.position += new Vector3(-(speed / 100), (speed / 200), 0);
+            sr.sprite = upLeft;
+        }
+        else if (direction == "DL")
+        {
+            transform.position += new Vector3(-(speed / 100), -(speed / 200), 0);
+            sr.sprite = downLeft;
+        }
     }
 }
