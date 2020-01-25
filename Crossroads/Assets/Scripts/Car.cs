e8a1920da7a44d14ca4397ddafd21e3f87f81015
Micheal Quentin <mquenti3@gmail.com>
2020-01-24 23:10:59 -0600

    Car Stops at Intersection

--- Crossroads/Assets/Scripts/Car.cs
+++ Crossroads/Assets/Scripts/Car.cs
@@ -8,6 +8,8 @@ public class Car : MonoBehaviour
     public float maxSpeed;
     public string direction;
     public bool canGo;
+    public bool stopped;
+    public float distanceFromIntersection;
 
     // Sprites
     public SpriteRenderer sr;
@@ -22,6 +24,7 @@ public class Car : MonoBehaviour
         speed = 0;
         maxSpeed = 9f;
         canGo = true;
+        stopped = false;
 
         if (transform.position.x < 0 && transform.position.y >= 0)
         {
@@ -44,6 +47,9 @@ public class Car : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        distanceFromIntersection = Vector3.Distance(transform.position, Vector3.zero);
+
+        // Move and Stop the car
         if (canGo == true)
         {
             if (speed < maxSpeed)
@@ -69,25 +75,46 @@ public class Car : MonoBehaviour
             }
         }
 
+        // Point the car in the correct direction
         if (direction == "DR")
         {
             transform.position += new Vector3((speed / 100), -(speed / 200), 0);
             sr.sprite = downRight;
+
+            if (Vector3.Distance(transform.position, Vector3.zero) < 3.82)
+            {
+                canGo = false;
+            }
         }
         else if (direction == "UR")
         {
             transform.position += new Vector3((speed / 100), (speed / 200), 0);
             sr.sprite = upRight;
+
+            if (Vector3.Distance(transform.position, Vector3.zero) < 3.7)
+            {
+                canGo = false;
+            }
         }
         else if (direction == "UL")
         {
             transform.position += new Vector3(-(speed / 100), (speed / 200), 0);
             sr.sprite = upLeft;
+
+            if (Vector3.Distance(transform.position, Vector3.zero) < 3.6)
+            {
+                canGo = false;
+            }
         }
         else if (direction == "DL")
         {
             transform.position += new Vector3(-(speed / 100), -(speed / 200), 0);
             sr.sprite = downLeft;
+
+            if (Vector3.Distance(transform.position, Vector3.zero) < 2.8)
+            {
+                canGo = false;
+            }
         }
     }
 }
